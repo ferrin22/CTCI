3575884b2abf0d16231c2e6b142d80063f6596e7
Michael Ferrin <mferrin@berkeley.edu>
2017-07-02 17:17:19 -0600

    coded 1.5

---
+++ 1-5.py
@@ -0,0 +1,22 @@
+#Peform basic string compression using the counts of repeated characters
+#Solution: Loop O(n)
+def compressor(string):
+	counter = 1
+	compressed = []
+	current = string[0]
+	compressed.append(current)
+	for i in range(1, len(string)):
+		if string[i] == current:
+			counter += 1
+		else:
+			compressed.append(counter)
+			counter = 1
+			current = string[i]
+			compressed.append(current)
+	compressed.append(counter)
+	if len(compressed) > len(string):
+		return string
+	return compressed
+
+
+print compressor("aabcccccaaa")
